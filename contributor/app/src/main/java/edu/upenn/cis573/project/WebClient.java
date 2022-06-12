565bfe9148c6439327874ee574e0c1f7219daff7
Chris Murphy <cdmurphy@seas.upenn.edu>
2022-06-12 14:56:35 +0900

    changed AsyncTask to Executor

--- contributor/app/src/main/java/edu/upenn/cis573/project/WebClient.java
+++ contributor/app/src/main/java/edu/upenn/cis573/project/WebClient.java
@@ -6,12 +6,17 @@ import android.util.Log;
 import java.net.URL;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
-public class WebClient {
+public class WebClient implements Runnable {
 
     private String host;
     private int port;
+    protected String response;
+    protected String request;
 
     public WebClient(String host, int port) {
         this.host = host;
@@ -30,55 +35,55 @@ public class WebClient {
      */
     public String makeRequest(String resource, Map<String, Object> queryParams) {
 
-        String request = "http://" + host + ":" + port + resource + "?";
+        request = "http://" + host + ":" + port + resource + "?";
 
         for (String key : queryParams.keySet()) {
             request += key + "=" + queryParams.get(key) + "&";
 
         }
-
-        //Log.v("webclient", request);
+        Log.v("webclient", request);
 
         /*
         Web traffic must be done in a background thread in Android.
-        This approach is deprecated but this should still work!
          */
-        AsyncTask<String, String, String> asyncTask = new AsyncTask<String, String, String>() {
-            @Override
-            protected String doInBackground(String... requests) {
-                try {
-                    URL url = new URL(requests[0]);
-                    url.openConnection();
-                    Scanner in = new Scanner(url.openStream());
-                    String response = "";
-                    while (in.hasNext()) {
-                        String line = in.nextLine();
-                        response += line;
-                    }
-
-                    in.close();
-                    return response;
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-        };
-
-        asyncTask.execute(request);
         try {
-            String response = asyncTask.get();
-            //Log.v("webclient", response);
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(this);
+
+            // this waits for up to 2 seconds
+            // it's a bit of a hack because it's not truly asynchronous
+            // but it should be okay for our purposes (and is a lot easier)
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+
+            Log.v("webclient", response);
             return response;
         }
         catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return e.toString();
         }
+    }
 
 
 
+    @Override
+    public void run() {
+        try {
+            URL url = new URL(request);
+            url.openConnection();
+            Scanner in = new Scanner(url.openStream());
+            response = "";
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                response += line;
+            }
+
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            response = e.toString();
+        }
     }
 
 
